80d97fc05e3570fac3b95906f9585c86d0c089b6
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2025-05-08 00:04:15 -0300

    removed forced resolution size

--- Scripts/System/CQualitySettings.cs
+++ Scripts/System/CQualitySettings.cs
@@ -6,33 +6,6 @@ using Screen = UnityEngine.Device.Screen;
 namespace EnigmaCore {
     public static class CQualitySettings {
 
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
-        static void InitializeAfterSceneLoad() {
-
-			#if UNITY_STANDALONE
-			StaticRoutinesRunner.RunRoutine(MonitorResolution());
-			IEnumerator MonitorResolution()
-        	{
-				if(Application.isEditor) yield break;
-            	Debug.Log($"Starting monitoring for screen resolution smaller than 1024x768 to resize.");
-            	while (true) {
-                	yield return null;
-	                const int targetWidth = 1024;
-	                const int targetHeight = 768;
-              	  	if (Screen.fullScreenMode == FullScreenMode.Windowed) {
-	                    bool setWidth = Screen.width < 1024;
-	                    bool setHeight = Screen.height < 768;
-	                    if(setWidth || setHeight) {
-		                    Screen.SetResolution(setWidth ? targetWidth : Screen.width, setHeight ? targetHeight : Screen.height,false);
-	                    }
-                    }
-           	 	}
-        	}
-			#endif
-        }
-
-
-
         public static bool GetVsync() {
             return QualitySettings.vSyncCount == 1;
         }
