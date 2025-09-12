b1a1006cb58bddc42fd76331b51777181c5f4edf
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2025-09-12 09:11:44 -0300

    StateUnityEvents

---
+++ Scripts/Utils/StateUnityEvents.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace EnigmaCore
+{
+    /// <summary>
+    /// A collection of UnityEvents for handling state changes.
+    /// </summary>
+    [Serializable]
+    public class StateUnityEvents
+    {
+        [SerializeField] UnityEvent On = new();
+        [SerializeField] UnityEvent Off = new();
+        [SerializeField] CUnityEventBool State = new();
+        [SerializeField] CUnityEventBool StateInverted = new();
+
+        public void Trigger(bool value)
+        {
+            State.Invoke(value);
+            StateInverted.Invoke(!value);
+            if (value)
+                On.Invoke();
+            else
+                Off.Invoke();
+        }
+    }
+}
