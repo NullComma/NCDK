797eb0c8e35b2a325940c5d16d250cf3062c31f6
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2025-09-19 14:19:49 -0300

    EventEmitterExtensions

---
+++ Scripts/Extensions/EventEmitterExtensions.cs
@@ -0,0 +1,20 @@
+﻿#if FMOD
+using FMODUnity;
+
+namespace EnigmaCore {
+    public static class EventEmitterExtensions {
+        
+        /// <summary>
+        /// Returns the path of the event reference if in editor, otherwise returns the GUID as string, because path is not available in builds.
+        /// </summary>
+        public static string GetPathSafe(this EventReference eventReference) {
+            #if UNITY_EDITOR
+            return eventReference.Path;
+            #else
+            return eventReference.Guid.ToString();
+            #endif
+        }
+        
+    }
+}
+#endif
