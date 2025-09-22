a6e84a01623799eb3b07ccbd8098e3424e6fa283
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2025-09-22 12:55:01 -0300

    PlayableDirectorExtensions

---
+++ Scripts/Extensions/PlayableDirectorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.Playables;
+
+namespace EnigmaCore
+{
+    public static class PlayableDirectorExtensions
+    {
+        /// <summary>
+        /// Condition is considered using playing time if bigger than 0f and minor than its total duration.
+        /// </summary>
+        public static bool IsPlaying(this PlayableDirector playableDirector)
+        {
+            return playableDirector.time > 0f && playableDirector.time <= playableDirector.duration;
+        }
+    }
+}
