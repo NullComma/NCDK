43049ba0b9d0f1407d89ce8b57e0fbce8dc31d11
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2025-11-22 21:10:15 -0300

    TimedAutoTriggerRepeating

---
+++ Scripts/GameObjects/Triggers/TimedAutoTriggerRepeating.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#endif
+
+namespace EnigmaCore
+{
+    public class TimedAutoTriggerRepeating : CAutoTriggerCompBase
+    {
+        [SerializeField]
+#if ODIN_INSPECTOR
+        [MinMaxSlider(0f, 60f, true)]
+#endif
+        private Vector2 _repeatInterval = new Vector2(1f, 5f);
+
+        [SerializeField]
+        private UnityEvent _onRepeatTrigger = new();
+
+        protected override void TriggerEvent()
+        {
+            StartCoroutine(RepeatRoutine());
+        }
+
+        private IEnumerator RepeatRoutine()
+        {
+            while (enabled)
+            {
+                float waitTime = Random.Range(_repeatInterval.x, _repeatInterval.y);
+                yield return new WaitForSeconds(waitTime);
+
+                ExecuteRepeat();
+            }
+        }
+
+        protected virtual void ExecuteRepeat()
+        {
+            _onRepeatTrigger.Invoke();
+        }
+    }
+}
