e7738ea6bb9082c1d8fa6b2cc11130489e535c8d
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2026-01-03 23:16:12 -0300

    AssetOnlyAttribute

---
+++ Scripts/Attributes/AssetOnlyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using UnityEngine;
+
+namespace EnigmaCore
+{
+    [AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+    public class AssetOnlyAttribute : PropertyAttribute { }
+}
