76b256edecb8fdac49bca407587fb0f6f72f6130
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2026-04-01 00:14:30 -0300

    Add SceneReloader component

---
+++ Scripts/System/Scenes/SceneReloader.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace EnigmaCore {
+	public class SceneReloader : MonoBehaviour {
+		
+		public void ReloadCurrentScene() {
+			var activeScene = SceneManager.GetActiveScene();
+			SceneManager.LoadScene(activeScene.buildIndex);
+		}
+		
+		public void ReloadCurrentSceneAsync() {
+			var activeScene = SceneManager.GetActiveScene();
+			SceneManager.LoadSceneAsync(activeScene.buildIndex);
+		}
+	}
+}
