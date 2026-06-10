ca95a7c6c691fbb9fb71f9563ea9eecfb2211eaf
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2026-06-09 23:49:28 -0300

    feat: add FMOD encryption key to ObfuscatedConstants

--- Scripts/System/ObfuscatedConstants.cs
+++ Scripts/System/ObfuscatedConstants.cs
@@ -11,6 +11,9 @@ namespace NCDK
 
         const uint DemoSteamMask = 0xAB85F098u; // 4776780 ^ SteamKey
 
+        static readonly byte[] FmodKey = { 75, 51, 121, 70, 48, 114, 70, 77, 48, 68, 51, 110, 99, 33, 46, 46 };
+        static readonly byte[] FmodMask = { 17, 109, 74, 48, 19, 37, 48, 19, 117, 110, 67, 39, 86, 21, 74, 94 };
+
         [MethodImpl(MethodImplOptions.NoInlining)]
         public static uint DecodeSteamAppId(bool isDemo)
         {
@@ -23,6 +26,12 @@ namespace NCDK
         public static uint DecodeUint(uint masked, uint key)
         {
             return masked ^ key;
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static string DecodeFmodKey()
+        {
+            return DecodeString(FmodMask, FmodKey);
         }
 
         [MethodImpl(MethodImplOptions.NoInlining)]
